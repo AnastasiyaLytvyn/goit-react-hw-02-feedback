eb0d87f6fb649930f8f366319961aec9be392a5b
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-12-16 13:34:44 +0200

    add component

---
+++ src/components/feedback/feedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Feedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
+
+      render() {
+        return <div>Hello world!</div>;
+      }
+}
