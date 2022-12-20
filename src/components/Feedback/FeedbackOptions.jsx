f52803c202a340e2f1cbceffa482caba13c85111
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-12-20 21:01:03 +0200

    update

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './Feedback.module.css';
+import css from './FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
